37ba6a7bd1910893899372ca27389a373a211f4a
Andrew <89681138+Kyushuki@users.noreply.github.com>
2024-10-03 11:55:31 +0300

    Работает не совсем как надо
    распознает цифры, на которых обучен, но также "распознает" цифры которых не знает
    5 - видит 2 и 4
    6 - видит 2 и 4

--- neuron.py
+++ neuron.py
@@ -16,30 +16,57 @@ class Neuron:
 class NeuralNetwork:
     def __init__(self):
         self.n1 = Neuron()
+        self.n2 = Neuron()
+        self.n3 = Neuron()
+        self.n4 = Neuron()
         self.temp = 0.01
         self.d = 0
-    def train(self, input_set, output_set, epoch):
+    def train(self, input_set, output_set, epoch, neuron : Neuron):
+        print("epoch start")
         # задание смещения и весов 
-        self.n1.bias = randint(1,3)
-        print(self.n1.bias)
+        neuron.bias = randint(1,3)
         for i in range(len(input_set[0])):
-            self.n1.weight.append(random())
-        print(self.n1.weight)
+            neuron.weight.append(random())
         # начало тренировки по эпохам
         for k in range(epoch):
             # для каждого элемента из набора данных
             for i in range(len(input_set)):
-                net = self.n1.think(input_set[i])
+                net = neuron.think(input_set[i])
                 error = output_set[i] - net
-                ad = error * self.n1.activate_der(net)
-                if k % 1000 == 0:
-                    print(ad)
-                    print(net)
+                ad = error * neuron.activate_der(net)
+                # if k % 1000 == 0:
+                #     print(ad)
+                #     print(net)
                 for j in range(len(input_set[0])):
-                    self.n1.weight[j] += ad*self.temp*input_set[i][j]
-                    self.n1.bias += ad*input_set[i][j]
-        print(self.n1.weight)
+                    neuron.weight[j] += ad*self.temp*input_set[i][j]
+                    neuron.bias += ad*input_set[i][j]
         print("epoch's end")
+    def train_net(self, input, output):
+        self.train(input, output[0], 10000, self.n1)
+        self.train(input, output[1], 10000, self.n2)
+        self.train(input, output[2], 10000, self.n3)
+        self.train(input, output[3], 10000, self.n4)
+        print("train over")
+    def think(self, input):
+        n1 = self.n1.think(input)
+        print(n1)
+        n2 = self.n2.think(input)
+        print(n2)
+        n3 = self.n3.think(input)
+        print(n3)
+        n4 = self.n4.think(input)
+        print(n4)
+        if n1 >= 0.8:
+            print("it's 1")
+        if n2 >= 0.8:
+            print("it's 2")
+        if n3 >= 0.8:
+            print("it's 4")
+        if n4 >= 0.8:
+            print("it's 7")
+        if n1 < 0.8 and n2 < 0.8 and n3 < 0.8 and n4 < 0.8:
+            print("i dont know such number")
+
 n = NeuralNetwork()
 # iset = [[0,1,1],[1,1,1],[0,0,1],[1,0,0]]
 # oset = [1,0,1,0]
@@ -62,10 +89,25 @@ iset = [[0,0,0,1,0,0,0,1,1,0,0,1,0,1,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0],
 [0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1],
 [1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0],
 [1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0]]
-oset = [0,0,0,1]
-n.train(iset,oset,10000)
+oset = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+n.train_net(iset, oset)
+# 1 2 4 7 5 6
+iset = [[0,0,0,1,0,0,0,1,1,0,0,1,0,1,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0],
+[0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1],
+[1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0],
+[1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0],
+[1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1],
+[1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1]]
 for i in range(len(iset)):
-    print(n.n1.think(iset[i]))
+    print(i)
+    n.think(iset[i])
+# oset = [0,0,0,1]
+# n.train(iset,oset,10000)
+# for i in range(len(iset)):
+#     print(n.n1.think(iset[i]))
+
+
+
 # [0,0,0,0,0,
 #  0,0,0,0,0,
 #  0,0,0,0,0,
